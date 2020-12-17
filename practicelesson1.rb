bf3cad215f7f2a5f6b691a4b85d8f1acdde80b3c
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-16 20:44:20 -0500

    practicing adding, deleting and a couple of other commands associated with arrays

---
+++ practicelesson1.rb
@@ -0,0 +1,48 @@
+#creating an array
+my_array = ["monkeys", "dogs", "chickens", "cows", "horses"]
+
+#adding to an array 
+#method 1
+my_array.push ("pigs")
+#method 2
+my_array << "lions"
+
+#removing from an array 
+#method 1-removing the last element on the list
+my_array.pop
+my_array.pop(2)#the number removes that many elements from the array at the end of the array
+#method 2- removing a specific item from the list
+my_array.delete("monkeys")
+#method 3- deleting based on index position
+my_array.delete_at(0)
+
+
+new_array = [1, 2, 3, 4, 5,6, 7, 8,]
+new_array.push 10
+new_array << 11
+new_array.pop(4)
+new_array.delete(7)
+new_array.delete_at(0)
+
+
+
+# #accesing elements in an array
+puts new_array[3]
+puts new_array[-2]
+
+
+#gives the length of the array
+puts new_array.length
+
+#range within an array
+puts new_array [0..2]
+
+#modifying array values
+new_array(-1) = "9"
+puts new_array
+
+
+
+
+
+
