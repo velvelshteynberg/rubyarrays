c929912c45679fe393170b96f818fc113ea090ad
Velvelshteynberg <Velvelshteynberg@outlook.com>
2020-12-19 19:57:39 -0500

    learning hashes

---
+++ lesson2(hashes).rb
@@ -0,0 +1,68 @@
+# #hashes are a collection of unique keys and their values: are defined in curly brackets: a symbol is a static string defined as :"name of symbol"
+# # a string is within brackwts
+# #string.split splits into the seperte components
+# # two ways to create array. array = [] or array.new 
+# #two ways to create a hash hash={} or hash.new
+# hash = {"bricks" => "red"} 
+# #bricks is the key and red is the value. 
+# # => is called hash rocket
+#  #only the key can be string and symbol
+# # hash person = {"first_name"=> john", "last_name"=>"smith"}
+
+#  light = {:type=>"led", :price=>1.99}
+
+#  #shortcut
+#  light = {type: "led", price: 1.99}
+
+#  person = {first_name: "velvel", last_name: "shteynberg"}
+
+#  #adding a key
+#  person[:num] = "9999" (num: is the key and 9999 is the value)
+
+#  #accesing value
+#  person[:first_name]
+
+#  #modifying value
+#  person[:first_name]="Beniomin"
+
+#  #deleting a key
+#  person.delete(:first_name)
+
+#  #you can have hashes within hashes
+#  # to access values type person[:person][:type]
+
+#  #to know the keys that belong to a hash type person.keys(will return the info in an array)
+
+#  #to know the values that belong to a hash type person.values (will return values in an array)
+
+#  #arrays are used when all the data types are the same. hashes are used when they are different
+
+# #iteration is processing every element in hash/array e.g. loops
+
+# 10.times do |my_num|
+#     puts "you are in my #{my_num}"
+# end
+
+# #iterating through a collection
+# arrays = ["chocolate", "vanilla", "strawberry", "honeycomb"]
+
+# arrays.each do |flavour|
+#     puts "favoroutie flavour is #{flavour}!!!"
+# end 
+
+# #to create an array from a range type 
+numbers = (1..10).to_a
+# numbers.each do |number|
+#     puts "you get $#{number*100}"
+# end 
+
+# #to excecute .each the collection does not need to be saved as a variable. it can just be typed in and put in .each afterwards
+
+# #another way to execute .each
+# numbers.each {|number| puts "you get $#{number*999}"}
+
+#learning another loop .map
+new_numbers = numbers.map do |number|
+    number * 1000
+end 
+puts new_numbers
---
+++ practicelesson2.rb
@@ -0,0 +1,51 @@
+#method 1 to create a hash
+people = {"first name" => "velvel", "last name"=> "shteynberg"}
+#method 2 to create a hash
+people = {:first_name => "velvel", :last_name => "shteynberg"}
+#method 3 to create a hash (shortcut and easiest way to create a shortcut)
+people = {first_name: "velvel", last_name: "shteynberg", age: 23}
+
+#adding a key with a value to a hash
+people[:fav_num] = 100
+
+#deleting a key
+people.delete(:fav_num)
+
+#accesing value
+people[:first_name]
+
+#accesing hashes withing hashes just requires a double square bracket
+
+#modifying values 
+people[:first_name] = "Beniomin"
+
+
+#to find all of the key values 
+people.keys
+
+#to find all of the values 
+people.values
+
+#creating an array from a range
+numbers = (1..100).to_a
+
+#practicing a standard loop
+10.times do |my_num|
+    puts "you are in my #{my_num}"
+end
+
+#the .each loop
+# numbers.each |number|
+#     puts "we are holding by number #{number}"
+# end
+
+#another method to execute the .each loop
+numbers.each {|number| "we are holding by number #{number}"}
+
+puts numbers
+
+
+
+
+
+#questions 1-line 37-40 are not working
